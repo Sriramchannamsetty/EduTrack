2365347e53cf215ea48bbd3b5ed5f8d0fd3e952c
Sriramchannamsetty <channamsettysriram@gmail.com>
2025-02-04 20:15:45 +0530

    authentication backend

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,4 +1,91 @@
-const signup=()=>{};
-const login=()=>{};
-const logout=()=>{};
+const generatetoken=require("../utils/generateToken");
+const User=require("../models/User");
+const bcrypt=require("bcryptjs");
+const signup=async (req,res)=>{
+    try{
+      const {username,password,email,name,role}=req.body;
+    //   console.log(username,password,email,name,role);
+      const emailRegex= /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if(!emailRegex.test(email)){
+        return res.status(400).json({error: "email is not valid"});
+      }
+      const existingUser=await User.findOne({username});
+     
+      if(existingUser){
+        return res.status(400).json({error: "user already exist"});
+      }
+     
+      const salt=await bcrypt.genSalt(10);
+      const hashedPassword=await bcrypt.hash(password,salt);
+      //console.log("hi ",hashedPassword);
+      const newUser=new User({
+        username,
+        password:hashedPassword,
+        name,
+        role,
+        email,
+      });
+       generatetoken(res,newUser._id);
+      await newUser.save();
+     
+     res.status(200).json({
+  _id: newUser._id,
+  username: newUser.username,
+  name: newUser.name,
+  email: newUser.email,
+  role: newUser.role,
+  profileImage: newUser.profileImage,
+  courses: newUser.courses,
+  assignments: newUser.assignments,
+  schedule: newUser.schedule,
+});
+
+    }
+    catch(err){
+        res.status(500).json({error:err});
+    }
+
+      
+
+};
+const login=async (req,res)=>{
+
+    try{
+   const {username,password}=req.body;
+   const existingUser= await User.findOne({username});
+   if(!existingUser){
+     return res.status(400).json({error:"username doesnot exist"});
+   }
+   const isPasswordCorrect=await bcrypt.compare(password,existingUser.password)
+   if(!isPasswordCorrect){
+     return res.status(400).json({error:"incorrect password"});
+   }
+   generatetoken(res,existingUser._id);
+   res.status(200).json({
+    _id: existingUser._id,
+    username: existingUser.username,
+    name: existingUser.name,
+    email: existingUser.email,
+    role: existingUser.role,
+    profileImage: existingUser.profileImage,
+    courses: existingUser.courses,
+    assignments: existingUser.assignments,
+    schedule: existingUser.schedule,
+  });
+  }
+  catch(err){
+    res.status(500).json({error:"server error"});
+  }
+
+};
+const logout=async(req,res)=>{
+
+    try {
+		res.cookie("jwt", "", { maxAge: 0 });
+		res.status(200).json({ message: "Logged out successfully" });
+	} catch (error) {
+		console.log("Error in logout controller", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
+};
 module.exports={signup,login,logout};
--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,16 @@ const express=require("express");
 const app=express();
 const mongoose=require("mongoose");
 const authRoutes=require("./routes/auth.routes");
+const cookieParser = require("cookie-parser");
 const connectdb = async()=>{
    await mongoose.connect(process.env.MONGO_URL);
 }
 connectdb().then( console.log("database connected"));
 
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.json());
 
-app.get("/",(req,res)=>{res.send("welcome to edutrack");});
+app.get("/home",(req,res)=>{res.send("welcome to edutrack");});
 app.use("/api/auth",authRoutes);
 app.listen( process.env.PORT,()=>{console.log("listening at port ",process.env.PORT);})
